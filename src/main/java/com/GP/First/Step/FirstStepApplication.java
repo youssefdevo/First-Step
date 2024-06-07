0889ccbac82f268c3a260a89f86a197ddb71831a
Youssef Nasser <y556217@gmail.com>
2024-06-07 18:11:45 +0300

    test deployment again

--- src/main/java/com/GP/First/Step/FirstStepApplication.java
+++ src/main/java/com/GP/First/Step/FirstStepApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FirstStepApplication {
 
-    @GetMapping("/message")
+    @GetMapping("/test")
     public String message() {
         return "OK Finally ?";
     }
