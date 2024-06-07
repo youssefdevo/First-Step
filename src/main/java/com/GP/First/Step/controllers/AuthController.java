4f8d373c8b732cff39f79f19bfff9d1b39be39c9
Youssef Nasser <y556217@gmail.com>
2024-06-07 20:18:28 +0300

    Add controllers and solve pom file

---
+++ src/main/java/com/GP/First/Step/controllers/AuthController.java
@@ -0,0 +1,74 @@
+package com.GP.First.Step.controllers;
+
+import com.GP.First.Step.DAO.UserRepository;
+import com.GP.First.Step.DTO.request.LoginReq;
+import com.GP.First.Step.DTO.response.ErrorRes;
+import com.GP.First.Step.DTO.response.LoginRes;
+import com.GP.First.Step.auth.JwtUtil;
+import com.GP.First.Step.entities.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/rest/auth")
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private JwtUtil jwtUtil;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtUtil jwtUtil, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity login(@RequestBody LoginReq loginReq) {
+
+        try {
+            Authentication authentication =
+                    authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginReq.getEmail(), loginReq.getPassword()));
+            String email = authentication.getName();
+
+
+            User user = userRepository.findByEmail(email).orElseThrow(() -> new BadCredentialsException("Invalid credentials"));
+            System.out.println(user);
+            String token = jwtUtil.createToken(Optional.ofNullable(user));
+            LoginRes loginRes = new LoginRes(email, token);
+
+            return ResponseEntity.ok(loginRes);
+
+        } catch (BadCredentialsException e) {
+            ErrorRes errorResponse = new ErrorRes(HttpStatus.BAD_REQUEST, "Invalid username or password");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        } catch (Exception e) {
+            ErrorRes errorResponse = new ErrorRes(HttpStatus.BAD_REQUEST, e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+    }
+
+    @ResponseBody
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody User user) {
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorRes(HttpStatus.CONFLICT, "Email already in use"));
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
---
+++ src/main/java/com/GP/First/Step/controllers/ProfileController.java
@@ -0,0 +1,73 @@
+package com.GP.First.Step.controllers;
+
+
+import com.GP.First.Step.DAO.UserRepository;
+import com.GP.First.Step.DTO.request.ResetPasswordReq;
+import com.GP.First.Step.auth.TokenBlacklist;
+import com.GP.First.Step.entities.User;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/rest/profile")
+public class ProfileController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenBlacklist tokenBlacklist;
+
+    public ProfileController(UserRepository userRepository, PasswordEncoder passwordEncoder, TokenBlacklist tokenBlacklist) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.tokenBlacklist = tokenBlacklist;
+    }
+
+    @GetMapping
+    public ResponseEntity<User> getProfile() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found"));
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping
+    public ResponseEntity<User> updateProfile(@RequestBody User updatedUser) {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found"));
+
+        user.setFirstName(updatedUser.getFirstName());
+        user.setLastName(updatedUser.getLastName());
+        user.setUserName(updatedUser.getUserName());
+        userRepository.save(user);
+
+        return ResponseEntity.ok(user);
+    }
+
+    @PutMapping("/reset-password")
+    public ResponseEntity<?> resetPassword(@RequestBody ResetPasswordReq resetPasswordReq) {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        System.out.println(email);
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!passwordEncoder.matches(resetPasswordReq.getCurrentPassword(), user.getPassword())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Current password is incorrect");
+        }
+
+        user.setPassword(passwordEncoder.encode(resetPasswordReq.getNewPassword()));
+        userRepository.save(user);
+
+        return ResponseEntity.ok("Password reset successfully");
+    }
+
+    @PostMapping("/signout")
+    public ResponseEntity<?> signOut(HttpServletRequest request) {
+        String token = request.getHeader("Authorization").substring(7);
+        tokenBlacklist.add(token);
+        SecurityContextHolder.clearContext();
+        return ResponseEntity.ok("User signed out successfully");
+    }
+
+}
