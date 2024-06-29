8cdddae56bad0b279425a7644fd722c2f17e8b49
TokaHamdy <doudyhamdy.th@gmail.com>
2024-06-30 00:24:01 +0300

    comment each part for user controller & service

--- src/main/java/com/GP/First/Step/controllers/UserController.java
+++ src/main/java/com/GP/First/Step/controllers/UserController.java
@@ -3,9 +3,9 @@ package com.GP.First.Step.controllers;
 import com.GP.First.Step.entities.User;
 import com.GP.First.Step.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;     // Annotation for dependency injection
+import org.springframework.web.bind.annotation.PathVariable;   // Imports the PathVariable annotation to extract data from the URI
+import org.springframework.web.bind.annotation.RequestMapping; // Imports the RequestMapping annotation to map web requests
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,14 +14,17 @@ public class UserController {
 
     private final UserService userService;
 
+    // Indicates that this constructor should be used for dependency injection
     @Autowired
     public UserController(UserService userService) {
-        this.userService = userService;
+
+        this.userService = userService;  // Initializes the userService instance
     }
 
+   // Retrieve a User entity by its ID
     @GetMapping("/{userId}")
     public User getUserById(@PathVariable int userId) {
         return userService.getUserById(userId)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+                .orElseThrow(() -> new RuntimeException("User not found")); // Throws an exception if the user is not found
     }
 }
--- src/main/java/com/GP/First/Step/services/UserService.java
+++ src/main/java/com/GP/First/Step/services/UserService.java
@@ -2,33 +2,36 @@ package com.GP.First.Step.services;
 
 import com.GP.First.Step.DAO.UserRepository;
 import com.GP.First.Step.entities.User;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired; // annotation for dependency injection
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.Optional;// Imports the Optional class for handling potential null values
 
-@Service
+@Service //service component in Spring, making it eligible for component scanning and dependency injection
 public class UserService {
 
     private final UserRepository userRepository;
 
-    @Autowired
+    @Autowired // Indicates that this constructor should be used for dependency injection
     public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
+        this.userRepository = userRepository; // Initializes the userRepository instance
     }
-
+    // Retrieves a User by their email,using Optional to handle the case where no user is found
     public Optional<User> getUserByEmail(String email) {
         return userRepository.findByEmail(email);
     }
 
+    // Retrieves a User by their ID, using Optional to handle the case where no user is found
     public Optional<User> getUserById(int userId) {
         return userRepository.findById(userId);
     }
 
+    // Saves a User entity to the repository and returns the saved entity
     public User saveUser(User user) {
         return userRepository.save(user);
     }
 
+    // Deletes a User from the repository based using his ID
     public void deleteUser(int userId) {
         userRepository.deleteById(userId);
     }
